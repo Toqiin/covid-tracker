bfe74b1ab154d41b8658e64c6b7e06c156aeb1cf
Tim Mitchell <tmitchell722@gmail.com>
2020-12-23 03:51:29 -0500

    dependencies?

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core";
 
 function App() {
   return (
     <div className="app">
-      <h1>Test</h1>
+      <h1>COVID-19 Tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          
+          <MenuItem value="worldwide">Worldwide1</MenuItem>
+          <MenuItem value="worldwide">Worldwide2</MenuItem>
+          <MenuItem value="worldwide">Worldwide3</MenuItem>
+          <MenuItem value="worldwide">Worldwide4</MenuItem>
+
+        </Select>
+      </FormControl>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* Info Box */}
+      {/* Info Box */}
+      {/* Info Box */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
