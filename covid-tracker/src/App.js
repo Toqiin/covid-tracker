4a833b3ef317f91e6de2aecc4fef27d86168370c
Tim Mitchell <tmitchell722@gmail.com>
2020-12-23 04:06:44 -0500

    initial styling of header component and dropdown

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -4,21 +4,32 @@ import {
   FormControl,
   Select
 } from "@material-ui/core";
+import { useState } from 'react';
 
 function App() {
+
+  const [countries, setCountries] = useState(['USA', 'UK', 'India']); 
+
   return (
     <div className="app">
-      <h1>COVID-19 Tracker</h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value="abc">
-          
-          <MenuItem value="worldwide">Worldwide1</MenuItem>
-          <MenuItem value="worldwide">Worldwide2</MenuItem>
-          <MenuItem value="worldwide">Worldwide3</MenuItem>
-          <MenuItem value="worldwide">Worldwide4</MenuItem>
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+            
+            {/* <MenuItem value="worldwide">Worldwide1</MenuItem>
+            <MenuItem value="worldwide">Worldwide2</MenuItem>
+            <MenuItem value="worldwide">Worldwide3</MenuItem>
+            <MenuItem value="worldwide">Worldwide4</MenuItem> */}
 
-        </Select>
-      </FormControl>
+          </Select>
+        </FormControl>
+      </div>
+      
 
       {/* Header */}
       {/* Title + Select input dropdown field */}
